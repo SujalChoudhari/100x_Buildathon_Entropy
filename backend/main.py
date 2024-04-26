f4d987b60a8b46470d681e91be2a91488ba5d204
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-04-26 11:35:05 +0530

    chore: added chat router

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
 
+from routers.chat import router as chat_router
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# routers
+
+app.include_router(chat_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True)
