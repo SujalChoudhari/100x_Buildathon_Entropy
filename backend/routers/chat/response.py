7af5d3974c5e9620a3a09d37e069c047b3325e56
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-04-26 16:47:35 +0530

    fix: llm remembers now

--- backend/routers/chat/response.py
+++ backend/routers/chat/response.py
@@ -1,7 +1,6 @@
-from utils.chatbot import ChatBot
 
-def respond(chatbot,message: str):
+def respond(chatbot, message: str):
 
-    human_msg,ai_msg =  chatbot.invoke(proposal="Sale on laptops",text=message)
+    ai_msg = chatbot.invoke(text=message)
 
-    return ai_msg["content"]
+    return ai_msg
