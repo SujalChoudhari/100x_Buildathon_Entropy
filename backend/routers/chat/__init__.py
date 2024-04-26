c0c4f9cccec2d29f8aba8ebe732d6229e8760c08
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-04-26 15:28:48 +0530

    feat: Chatbot connected with API

--- backend/routers/chat/__init__.py
+++ backend/routers/chat/__init__.py
@@ -1,4 +1,8 @@
+from fastapi import Depends, Request
 from fastapi.routing import APIRouter
+
+from utils.chatbot import ChatBot
+from .response import respond
 
 router = APIRouter(
     prefix="/chat",
@@ -6,13 +10,24 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+chatbots = {}  
+
+
+def get_chatbot(request: Request):
+    # Retrieve or create a ChatBot for the current session
+    session_id = request.session.get("session_id")
+
+    if session_id not in chatbots:
+        chatbots[session_id] = ChatBot()
+
+    return chatbots[session_id]
+
 @router.get("/")
 async def check():
     return {"message": "Hello World"}
 
 
-@router.get("/response")
-async def response():
-    # call the LLM to generate a response
-    data = "hello world"
-    return {"response": data}
+@router.post("/response")
+async def response(query: str, chatbot=Depends(get_chatbot)):
+    query = query.strip()
+    return {"response": respond(chatbot,query)}
