4cb432a20b08868d1330c7d475c528200e49510f
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-04-27 14:04:14 +0530

    fix: fixed for current use

--- frontend/src/app/dashboard/chat/page.tsx
+++ frontend/src/app/dashboard/chat/page.tsx
@@ -1,4 +1,4 @@
-"use client";  
+"use client";
 import React, { useEffect, useState } from 'react'
 import ChatArea from '@/components/chat';
 import PromptBox from '@/components/PromptBox';
@@ -9,7 +9,6 @@ function page() {
 
   const [chatMessages, setChatMessages] = useState<string[]>([]);
   const [animatePrompt, setAnimatePrompt] = useState(false);
-  const [browserURL, setBrowserURL] = useState("");
   const [timeMs, setTimeMs] = useState(null);
 
 
@@ -38,52 +37,32 @@ function page() {
 
   // Example usage within onInputSent function
   const onInputSent = async (input: string) => {
-    const command = input.trim();
+    const userInput = input.trim();
     try {
-      const response = await axios.get(`http://127.0.0.1:80/chat/chat?message=${encodeURIComponent(command)}`);
-      setChatMessages(prevMessages => [...prevMessages, command, response.data.response]);
+      const response = await axios.post('http://localhost:8000/chat/response?query=' + encodeURIComponent(userInput));
+      setChatMessages(prevMessages => [...prevMessages, userInput, response.data.response]);
       console.log(response.data);
 
       // Convert and speak the first 50 words of the response
       speak(response.data.response, 50);
-
-      if (response.data.browser_url) {
-        setBrowserURL(response.data.browser_url);
-      }
       // Check if response.data.time_ms exists and set it
       if (response.data.time_ms) {
         setTimeMs(response.data.time_ms);
       }
       //   toast.success(`Successful interaction: ${timeMs}ms`)
     } catch (error) {
-      //   console.error('Error sending input:', error);
+        console.error('Error sending input:', error);
       //   toast.error("Failed to send Input. Try Again")
     }
   };
-
-
-  useEffect(() => {
-    const resetChat = async () => {
-      try {
-        await axios.get("http://127.0.0.1:80/chat/init");
-      } catch (error) {
-        console.error('Error resetting chat:', error);
-        // Handle error (if required)
-      }
-    };
-    resetChat();
-
-  }, []);
 
 
   return (
     <div>
       <Chat chatData={chatMessages.map((message, index) => ({ isUser: index % 2 === 0, message }))} />
       <PromptBox onSubmitPressed={onInputSent} animatePrompt={animatePrompt} setAnimatePrompt={setAnimatePrompt} timeMs={timeMs} />
-
-
     </div>
   )
 }
 
-export default Chat
+export default page
--- frontend/src/components/PromptBox.tsx
+++ frontend/src/components/PromptBox.tsx
@@ -10,17 +10,17 @@ import {
     Voicemail,
     X
 } from 'lucide-react';
+
+import { ScrollArea } from './ui/scroll-area';
 import { motion } from 'framer-motion';
 
+import { Button } from './ui/button';
+// import toast from 'react-hot-toast';
+import axios from 'axios';
 
 function PromptBox({ onSubmitPressed, animatePrompt, setAnimatePrompt, timeMs }: { onSubmitPressed: any, animatePrompt: boolean, setAnimatePrompt: any, timeMs: any }) {
-   
     const inputRef = useRef<HTMLInputElement | null>(null);
     const [isListening, setIsListening] = useState<boolean>(false);
-    const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-    const [items, setItems] = useState("");
-    const [isRecording, setIsRecording] = useState<boolean>(false);
-    const [data, setdata] = useState<any>({});
 
     useEffect(() => {
 
@@ -36,13 +36,13 @@ function PromptBox({ onSubmitPressed, animatePrompt, setAnimatePrompt, timeMs }:
                     inputRef.current.value = transcript;
                     onSubmitClicked();
                 }
-                toast.success("Voice Recognized")
+                // toast.success("Voice Recognized")
             };
 
             recognition.onerror = (event: any) => {
                 console.error('Speech recognition error:', event.error);
                 setIsListening(false);
-                toast.error(`Error: ${event.error}. Try moving to a quite place.`)
+                // toast.error(`Error: ${event.error}. Try moving to a quite place.`)
             };
 
             recognition.onend = () => {
@@ -61,78 +61,11 @@ function PromptBox({ onSubmitPressed, animatePrompt, setAnimatePrompt, timeMs }:
         const curr = inputRef.current;
         if (curr && curr.value) {
             onSubmitPressed(curr.value ?? '');
-            toast.success("Quering API")
+            // toast.success("Quering API")
             curr.value = '';
         }
     };
 
-    useEffect(() => {
-        const fn = async () => {
-            const res = await axios.get("http://127.0.0.1:80/userFile");
-            const file: string = res.data.content;
-            setItems(file);
-            console.log(file)
-        }
-        fn()
-    }, [])
-
-    const onMicSubmitClicked = async () => {
-        // if (audioBlob) {
-        //     const formData = new FormData();
-        //     formData.append('unknown_file', audioBlob, 'recording.wav');
-
-        //     const response = await axios.get('http://127.0.0.1:80/detection/predict/');
-
-        //     const data = response.data;
-        //     console.log('Predicted Speaker:', data.predicted_speaker);
-        //     console.log('Accuracy:', data.accuracy);
-        //     alert(`Predicted Speaker: ${data.predicted_speaker}\nAccuracy: ${data.accuracy}`);
-        // }
-
-        const randomSpeakerNames = ['F_saniyaa', 'M_sujal', 'M_soham', "guest0"];
-        const randomSpeaker = randomSpeakerNames[Math.floor(Math.random() * randomSpeakerNames.length)];
-        const randomAccuracy = Math.floor(Math.random() * 59); // Random accuracy between 0 and 100
-        setTimeout(() => {
-            const mockResponse = {
-                data: {
-                    predicted_speaker: randomSpeaker,
-                    accuracy: randomAccuracy
-                }
-            };
-
-            // Use the mock response instead of making an actual request
-            const data = mockResponse.data;
-            setdata(data);
-        }, randomAccuracy * 100)
-    };
-
-    const startRecording = async () => {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-            const mediaRecorder = new MediaRecorder(stream);
-
-            const chunks: BlobPart[] = [];
-            mediaRecorder.ondataavailable = (e) => {
-                chunks.push(e.data);
-            };
-
-            mediaRecorder.onstop = () => {
-                const audioBlob = new Blob(chunks, { type: 'audio/wav' });
-                setAudioBlob(audioBlob);
-            };
-
-            mediaRecorder.start();
-            setIsRecording(true);
-
-            setTimeout(() => {
-                mediaRecorder.stop();
-                stream.getTracks().forEach((track) => track.stop());
-                setIsRecording(false);
-            }, 3000); // Stop recording after 3 seconds
-        } catch (error) {
-            console.error('Error accessing microphone:', error);
-        }
-    };
 
     return (
         <motion.div
@@ -143,27 +76,32 @@ function PromptBox({ onSubmitPressed, animatePrompt, setAnimatePrompt, timeMs }:
         >
             <div id='inputs' className="mx-auto w-full sm:max-w-2xl sm:px-4">
                 <div className="bg-background  space-y-4 flex flex-row items-center gap-2  border-t px-4  shadow-lg sm:rounded-xl sm:border md:py-4 ">
-                    
+                    <div className="rounded-lg border shadow-md mt-4">
+                        <input ref={inputRef} placeholder="Input Prompt." />
+                    </div>
 
 
                     {/* ALL THE BUTTONS */}
 
 
                     <div id='voice_login'  className='  space-y-4 items-center justify-center flex flex-col'>
-                        
-
                         <div className='flex flex-row gap-4'>
-                            <button  className="group h-8 select-none rounded-lg bg-gradient-to-b from-zinc-800 via-zinc-700 to-zinc-600 px-3 text-sm leading-8 text-zinc-50 shadow-[0_-1px_0_1px_rgba(0,0,0,0.8)_inset,0_0_0_1px_rgb(9_9_11)_inset,0_0.5px_0_1.5px_#71717a_inset] hover:bg-gradient-to-b hover:from-zinc-900 hover:via-zinc-900 hover:to-zinc-700 active:shadow-[0_3px_0_0_rgba(0,0,0)_inset]">
+                            <button onClick={onSubmitClicked} className="group h-8 select-none rounded-lg bg-gradient-to-b from-zinc-800 via-zinc-700 to-zinc-600 px-3 text-sm leading-8 text-zinc-50 shadow-[0_-1px_0_1px_rgba(0,0,0,0.8)_inset,0_0_0_1px_rgb(9_9_11)_inset,0_0.5px_0_1.5px_#71717a_inset] hover:bg-gradient-to-b hover:from-zinc-900 hover:via-zinc-900 hover:to-zinc-700 active:shadow-[0_3px_0_0_rgba(0,0,0)_inset]">
                                 <span className="block group-active:[transform:translate3d(0,1px,0)]">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="white " className="size-4 text-white">
                                         <path d="M200 32v144a8 8 0 0 1-8 8H67.31l34.35 34.34a8 8 0 0 1-11.32 11.32l-48-48a8 8 0 0 1 0-11.32l48-48a8 8 0 0 1 11.32 11.32L67.31 168H184V32a8 8 0 0 1 16 0Z" />
                                     </svg>
                                 </span>
                             </button>
-                           
+                            <button onClick={() => { setIsListening(!isListening); /*toast.success("Start Speaking") */}} className="group h-8 select-none rounded-lg bg-gradient-to-b from-zinc-800 via-zinc-700 to-zinc-600 px-3 text-sm leading-8 text-zinc-50 shadow-[0_-1px_0_1px_rgba(0,0,0,0.8)_inset,0_0_0_1px_rgb(9_9_11)_inset,0_0.5px_0_1.5px_#71717a_inset] hover:bg-gradient-to-b hover:from-zinc-900 hover:via-zinc-900 hover:to-zinc-700 active:shadow-[0_3px_0_0_rgba(0,0,0)_inset]">
+                                <span className="block group-active:[transform:translate3d(0,1px,0)]">
+
+                                    <Mic size={15} />
+                                </span>
+                            </button>
                         </div>
 
-                        <button className="group block h-12 select-none rounded-lg bg-white px-3 text-sm  text-zinc-950 shadow-[0_-1px_0_0px_#d4d4d8_inset,0_0_0_1px_#f4f4f5_inset,0_0.5px_0_1.5px_#fff_inset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]"><span className="group-active:[transform:translate3d(0,1px,0)] text-sm flex flex-row items-center"> Response Time: </span></button>
+                        <button className="group block h-12 select-none rounded-lg bg-white px-3 text-sm  text-zinc-950 shadow-[0_-1px_0_0px_#d4d4d8_inset,0_0_0_1px_#f4f4f5_inset,0_0.5px_0_1.5px_#fff_inset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]"><span className="group-active:[transform:translate3d(0,1px,0)] text-sm flex flex-row items-center"> <Timer />Response Time: {timeMs ? `${(timeMs / 1000).toFixed(2)} sec` : 'N/A'}</span></button>
                     </div>
 
 
