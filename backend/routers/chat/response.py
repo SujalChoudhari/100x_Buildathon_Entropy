22234d43b76005cb64f32cf862e0108bfcdef0e4
siro844 <srinathreddy239@gmail.com>
2024-04-29 01:23:28 +0530

    fix :resources moved into their directory

--- backend/routers/chat/response.py
+++ backend/routers/chat/response.py
@@ -3,5 +3,4 @@ async def respond(chatbot, message: str):
     document_data = await query_index(message)
     print(document_data)
     ai_msg = chatbot.invoke(text=message,document_data=document_data[:400])
-    print(ai_msg)
     return ai_msg
