a3d9e3dfe4483dbe31c0081ce710401727e3d510
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-04-29 01:50:07 +0530

    fix: fixed test code

--- backend/main.py
+++ backend/main.py
@@ -368,8 +368,6 @@ async def root():
     """
     return {"message": "Hello World"}
 
-asyncio.run(cb.invoke("Hello", ""))
-
 # Run the server
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
